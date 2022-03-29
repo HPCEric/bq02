a2e9e15d06eb664385f174e0f18cf466159565ea
Mo <overy1399@gmail.com>
2022-03-29 11:47:58 +0800

    登入狀態判斷顯示不同按鈕

--- index.php
+++ index.php
@@ -40,7 +40,30 @@ include_once "base.php";
 
 					<marquee style="width:80%; display:inline-block;">請民眾踴躍投稿電子報</marquee>
 					<span style="width:18%; display:inline-block;">
-						<a href="?do=login">會員登入</a>
+
+						<?php
+						if (!isset($_SESSION['login'])) {
+						?>
+							<a href="?do=login">會員登入</a>
+							<?php
+						} else {
+							if ($_SESSION['login'] != 'admin') {
+							?>
+								<a href="?do=login">
+									歡迎，<?= $_SESSION['login']; ?>
+									<button onclick="logout()">登出</button>
+								</a>
+							<?php
+							} else {
+							?>
+								<a href="?do=login">
+									歡迎，<?= $_SESSION['login']; ?>
+									<button onclick="location.href='back.php'">管理</button>
+									<button onclick="logout()">登出</button>
+							<?php
+							}
+						}
+							?>
 					</span>
 					<div class="">
 						<!-- include -->
